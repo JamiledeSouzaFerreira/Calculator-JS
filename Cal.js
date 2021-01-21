a45097ea166a7ef367c60d6beb588e8853be5138
Jamile de Souza Ferreira <JamiledeSouzaFerreira@gmail.com>
2021-01-21 00:35:29 -0300

    ódio apenas

--- Cal.js
+++
@@ -1,8 +0,0 @@
-function calcular(operação){
-    var valor1 = document.calform.valor1.value
-    var valor2 = document.calform.valor2.value
-    if ( operação == "somar"){
-        var re
-
-    } 
-}
