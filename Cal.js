aa14a4037307ae57b97f0850e92adacd6e5fa223
Jamile de Souza Ferreira <JamiledeSouzaFerreira@gmail.com>
2021-01-20 21:36:55 -0300

    Começando a escrever a função em JS

--- Cal.js
+++ Cal.js
@@ -1,3 +1,8 @@
-function calcular (){   var oi = document.getElementById('area')
-    area.style
+function calcular(operação){
+    var valor1 = document.calform.valor1.value
+    var valor2 = document.calform.valor2.value
+    if ( operação == "somar"){
+        var re
+
+    } 
 }
